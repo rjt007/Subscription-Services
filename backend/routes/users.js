ffbce3ee6e2f84f56419371487d69b3ed282ee12
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-12 01:52:33 +0530

    backend/update users route to add api/subscribe endpoint for subscription plans

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const authorizeToken = require('../middleware/authorizeToken');
+const Plan = require('../models/plan');
 
 //User Registration
 router.post('/register', async(req,res)=>{
@@ -48,6 +51,65 @@ router.post('/login', async(req,res)=>{
     }
     catch(err){
         return res.status(500).json(err.message);
+    }
+});
+
+//User Subscription For a Plan
+router.post('/subscribe',authorizeToken,async(req,res)=>{
+    const {planId, stripeToken} = req.body;
+    if(!planId || !stripeToken){
+        return res.status(400).json('Error! either Id or Token is missing');
+    }
+    try{
+        const user = await User.findOne({_id:req.userId.id});
+        const plan = await Plan.findOne({_id:planId});
+
+        //Create a stripe customer
+        const customer = await stripe.customers.create({
+            name: user.name,
+            email: user.email,
+            payment_method: stripeToken,
+            invoice_settings: { default_payment_method: stripeToken }
+        });
+
+        // Create a product
+        const product = await stripe.products.create({
+            name: 'Subscription Plans'
+        });
+
+        //Create a subscription;
+        const subscription = await stripe.subscriptions.create({
+            customer: customer.id,
+            items: [
+                {
+                  price_data: {
+                    currency: "INR",
+                    product: product.id,
+                    unit_amount: plan.price*100,
+                    recurring: {
+                      interval: plan.interval==='monthly'?'month':'year',
+                    },
+                  },
+                },
+            ],
+            payment_settings: {
+                payment_method_types: ["card"],
+                save_default_payment_method: "on_subscription",
+            },
+            expand: ["latest_invoice.payment_intent"]
+        });
+
+        //Update user with subsciption details
+        await User.findByIdAndUpdate(req.userId.id, {subscription:{
+            planId: planId,
+            subscriptionId: subscription.id
+        }
+        });
+
+        res.status(201).json({plan,clientSecret: subscription.latest_invoice.payment_intent.client_secret});
+    }
+    catch(err){
+        res.status(500).json(err.message);
     }
 });
 
