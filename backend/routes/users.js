0405d81af2480ef4c9ff072ac948ee8373ac0aec
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-11 15:29:13 +0530

    backend/create routes/users for users registraion and login

---
+++ backend/routes/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+//User Registration
+router.post('/register', async(req,res)=>{
+    const {name,email,password} = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json('Error! All fields are required.');
+    }
+    try{
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = new User({
+            name:name,
+            email:email,
+            password:hashedPassword
+        });
+        await user.save();
+        return res.status(201).json({success:true});
+    }
+    catch(err){
+        return res.status(501).json(err.message);
+    }
+});
+
+//User Login
+router.post('/login', async(req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        return res.status(400).json('Error! All fields are required.');
+    }
+    try{
+        const user = await User.findOne({email:email});
+        if(user==null){
+            return res.status(400).json('Error! Enter a valid email.');
+        }
+        else if(!(await bcrypt.compare(password,user.password))){
+            return res.status(400).json('Error! Password entered is wrong.');
+        }
+        return res.status(201).json({success:true});
+    }
+    catch(err){
+        return res.status(500).json(err.message);
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const usersRoute = require('./routes/users');
+
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send('Hello world')
 });
+
+app.use('/api/user',usersRoute);
 
 //Database Connection
 const connectDB = require('./config/db');
